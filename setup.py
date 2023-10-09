741a187ef24498755532e2c4125301d0622b183b
Mark Radwin <markradwin@gmail.com>
2023-10-09 14:58:45 -0600

    readme readability

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Mark Radwin",
     author_email="markradwin@gmail.com",
     description="Python package simplifying large-scale operations using Google Earth Engine (GEE) for users who utilize Landsat and Sentinel",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/radwinskis/RadGEEToolbox",
     packages=find_packages(),
